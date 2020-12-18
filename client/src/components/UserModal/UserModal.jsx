086dc8e799472a39803ab57b83ddef5232048806
TheyKay <murraykyleb@gmail.com>
2020-12-17 21:37:16 -0700

    started adding react-bootstrap, globalcontext

--- client/src/components/UserModal/UserModal.jsx
+++ client/src/components/UserModal/UserModal.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import AboutMe from "../AboutMe";
 import Messages from "../Messages";
 import Matches from "../Matches";
+import Modal from "react-bootstrap/Modal"
 
 const UserModal = () => {
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header>
+        <Modal.Title></Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+
+      </Modal.Body>
+    </Modal>
+  )
 };
 
 export default UserModal;
