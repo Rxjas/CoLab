dcf43f5a989b16ab21b42f0c29bc8db4b957e483
TheyKay <murraykyleb@gmail.com>
2020-12-22 21:41:13 -0700

    started sidebar stuff

---
+++ client/src/components/FilterMusic/FilterMusic.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+const FilterMusic = () => {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default FilterMusic;
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,58 @@
 import React from "react";
-import UserModal from "../UserModal";
+import Form from "react-bootstrap/Form";
+// import FilterMusic from "../FilterMusic";
 
 const Sidebar = () => {
-  const displayUser = () => {
-    // display UserModal
-  };
-
   return (
     <>
-      <div id="userModal">{UserModal}</div>
       <div id="user">
         <h3>I am a...</h3>
-        <div id="autopop"
-          onClick={displayUser}
-        ></div>
+        <div id="autopop" />
       </div>
       <div id="seeking">
         <h3>looking to CoLaborate with...</h3>
-        <div id="filters">
-          <div id="filter1">placeholder element, replace with whatever is most appropriate</div>
-          <div id="filter2">placeholder element, replace with whatever is most appropriate</div>
-          <div id="filter3">placeholder element, replace with whatever is most appropriate</div>
-          <div id="filter4">placeholder element, replace with whatever is most appropriate</div>
-          <div id="filter5">placeholder element, replace with whatever is most appropriate</div>
-        </div>
+        {/* for more categories, need some way to make form change */}
+        {/* maybe a tab, like on profile page... or useState with another form (just a dropdown) */}
+        <Form>
+          <Form.Group controlId="genre">
+            <Form.Label>Genre</Form.Label>
+            <Form.Control as="select">
+              <option>Rock</option>
+              <option>Hip Hop</option>
+              <option>Pop</option>
+              <option>Jazz</option>
+              <option>Blues</option>
+              <option>Folk</option>
+              <option>Country</option>
+              <option>R&B</option>
+              <option>Classical</option>
+              <option>Soul</option>
+              <option>Metal</option>
+              <option>Punk</option>
+              <option>Indie</option>
+              <option>Electric</option>
+              <option>Dance</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="instrument">
+            <Form.Label>Instrument</Form.Label>
+            <Form.Control as="select">
+              <option>Vocals</option>
+              <option>Guitar</option>
+              <option>Piano</option>
+              <option>Bass</option>
+              <option>Saxophone</option>
+              <option>Percussion</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="other">
+    <Form.Label>Keywords</Form.Label>
+    <Form.Control as="textarea" rows={1} />
+    <Form.Text id="keywordHelp" muted>
+      Enter keywords (e.g., terms not included in filters) separated by spaces
+    </Form.Text>
+  </Form.Group>
+        </Form>
       </div>
     </>
   );
