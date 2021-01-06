df9b53f2ccd6bea7b4bd0499e00b399db37ba0f5
TheyKay <murraykyleb@gmail.com>
2021-01-05 21:35:01 -0700

    removed form text from Login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -9,9 +9,6 @@ const Login = () => {
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
