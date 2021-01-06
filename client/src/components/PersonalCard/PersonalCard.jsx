44eaf8f70fb13f9578518684527d60e836cf4b01
TheyKay <murraykyleb@gmail.com>
2021-01-05 20:45:31 -0700

    PersonalCard component progressing

--- client/src/components/PersonalCard/PersonalCard.jsx
+++ client/src/components/PersonalCard/PersonalCard.jsx
@@ -5,7 +5,15 @@ const PersonalCard = (props) => {
   return (
     <>
       <Card>
-        
+        <h3>{props.username} <span id="pronouns">{props.pronouns}</span></h3>
+        <h4>Roles</h4>
+        <ul>
+          {props.roles.map(role => {
+            return <li>{role}</li>
+          })}
+        </ul>
+        <h4>Looking for...</h4>
+        <p>{props.lookingfor}</p>
       </Card>
     </>
   )
