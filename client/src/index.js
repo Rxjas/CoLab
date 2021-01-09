dd1febdac4fd7600e5bb0e022aa104b882950b0c
Tony Rivas <tonyjrivas@gmail.com>
2021-01-09 13:20:04 -0700

    added registerServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,17 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Route, Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.render(
-    <BrowserRouter>
-    <Switch>
-    <Route path="/" component={App} /> 
-    </Switch>
-    </BrowserRouter>
-    document.getElementById("root")
-    );
-    registerServiceWorker();
+registerServiceWorker();
