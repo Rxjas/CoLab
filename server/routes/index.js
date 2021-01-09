5b926d9234b71ca6cedec96fb7c77c14345c0074
Tony Rivas <tonyjrivas@gmail.com>
2021-01-09 11:52:24 -0700

    implementing react import into index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const todos = require('./todo.routes');
 const pb = require('./pb.routes');
 
+//passport auth
+
+
 // /api/todo
 router.use('/api/todo', todos);
 router.use('/api/pubnub', pb);
