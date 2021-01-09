fcd0950e275045f68585ab8f80844c9bab8d4b0f
Tony Rivas <tonyjrivas@gmail.com>
2021-01-09 10:45:04 -0700

    passport auth

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
   );
 }
 
-// need to add this after the render function (from hackernoon.com
+// need to add this after the render function (from hackernoon.com by
 
 
 export default App;
