bdfe55d5eb729cb5a587eac5b5d7ee27f3db13c5
Tony Rivas <tonyjrivas@gmail.com>
2021-01-10 10:42:56 -0700

    added GET google auth API

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,12 @@ const pb = require('./pb.routes');
 
 var passport = require('passport');
 
+/* GET Google Authentication API. */
+router.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
 // /api/todo
 router.use('/api/todo', todos);
 router.use('/api/pubnub', pb);
