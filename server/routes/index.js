3b23c929e772d6ccad8b48cdf7aef9fc01c32c78
Tony Rivas <tonyjrivas@gmail.com>
2021-01-10 10:40:04 -0700

    removed unnecessary code from routes index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,27 +2,8 @@ const router = require('express').Router();
 const todos = require('./todo.routes');
 const pb = require('./pb.routes');
 
-//passport auth
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-
 // /api/todo
 router.use('/api/todo', todos);
 router.use('/api/pubnub', pb);
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Route, Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.render(
-    <BrowserRouter>
-    <Switch>
-    <Route path="/" component={App} /> 
-    </Switch>
-    </BrowserRouter>
-    document.getElementById("root")
-    );
-    registerServiceWorker();
 
 module.exports = router;
