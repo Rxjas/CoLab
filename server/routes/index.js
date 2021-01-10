32486fcf994bae2343b07818b5a64dee12be2ad8
Tony Rivas <tonyjrivas@gmail.com>
2021-01-10 10:46:25 -0700

    added heroku link to redirect

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,7 @@ router.get(
     passport.authenticate("google", { failureRedirect: "/", session: false }),
     function(req, res) {
         var token = req.user.token;
-        res.redirect("http://localhost:3000?token=" + token);
+        res.redirect("https://afternoon-eyrie-78094.herokuapp.com/?token=" + token);
     }
 );
 
