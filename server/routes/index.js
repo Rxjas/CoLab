51cb90609e6e84e1551fb527eb0ace603f73a8b3
Tony Rivas <tonyjrivas@gmail.com>
2021-01-10 10:42:18 -0700

    required passport in routes index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const todos = require('./todo.routes');
 const pb = require('./pb.routes');
 
+var passport = require('passport');
+
 // /api/todo
 router.use('/api/todo', todos);
 router.use('/api/pubnub', pb);
