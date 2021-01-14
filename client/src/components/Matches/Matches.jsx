5e4472faf0be2aca7dbd103ced5b8749dfe994a3
TheyKay <murraykyleb@gmail.com>
2021-01-14 15:30:27 -0700

    adding hooks to matches and personalcard for passing down and fetching/posting data

--- client/src/components/Matches/Matches.jsx
+++ client/src/components/Matches/Matches.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Matches.css";
+import PersonalCard from "../PersonalCard";
+import axios from "axios";
 
-const Matches = () => {
+const Matches = (props) => {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const route = `/api/user/matches/exampleUserNames`;
+    fetch(route)
+      .then((result) => {
+        console.log(result);
+        // setMatches(result);
+      })
+  })
+
   return (
     <>
       {/* <p>my matches yus</p> */}
       {/* put matches here */}
+      <PersonalCard 
+        username="musicman"
+        pronouns="he/him"
+        roles={["vocalist", "pianist", "guitarist"]}
+        lookingfor="a good singer"
+      />
+      <PersonalCard 
+        username="musicman"
+        pronouns="he/him"
+        roles={["vocalist", "pianist", "guitarist"]}
+        lookingfor="a good singer"
+      />
+      <PersonalCard 
+        username="musicman"
+        pronouns="he/him"
+        roles={["vocalist", "pianist", "guitarist"]}
+        lookingfor="a good singer"
+      />
+      <PersonalCard 
+        username="musicman"
+        pronouns="he/him"
+        roles={["vocalist", "pianist", "guitarist"]}
+        lookingfor="a good singer"
+      />
     </>
   )
 };
--- client/src/components/PersonalCard/PersonalCard.jsx
+++ client/src/components/PersonalCard/PersonalCard.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import "./PersonalCard.css";
+import axios from "axios";
 
 const PersonalCard = (props) => {
+  const [showBtn, setShowBtn] = useState(true);
+
+  // this variable stores the username of whoever is currently logged in
+  const currentUser = props.currentUser;
+  const path = window.location.pathname;
+  useEffect(() => {
+    if (path === "/grid") {
+      setShowBtn(true)
+    } else if (path === "/profile") {
+      setShowBtn(false);
+    }
+  }, [path])
+  // 
+  const addMatch = () => {
+    const route = `/api/user/match/${currentUser}`;
+    // const matches = props.username;
+    console.log(path)
+    axios
+      .put(route, { matches: props.username })
+      .catch(err => console.log(err));
+  }
+
   return (
     <>
       <Card>
@@ -15,6 +39,9 @@ const PersonalCard = (props) => {
         </ul>
         <h4>Looking for...</h4>
         <p>{props.lookingfor}</p>
+        {showBtn && (<Button
+          onClick={addMatch}
+        >add</Button>)}
       </Card>
     </>
   )
